733c0e6fb2b414153df4c3b01ddea3a1ce940563
islam-alsawaftah <islam.saw1996@yahoo.com>
2019-08-18 15:30:05 +0300

    ghh

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/*global swal, jQuery*/
+(function ($) {
+    "use strict";
+    
+
+ /*==================================================================
+    [ Focus input ]*/
+$('.container .col-lg-3').on('click', function() {
+$('.limiter').fadeToggle(2000);
+	})
+    
+ 
+
+
+})(jQuery);
